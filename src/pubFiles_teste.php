72ef90647be7df235c11849620270958c934ef3d
Peter Krauss <ppkrauss@gmail.com>
2016-10-13 22:49:31 -0300

    rename

---
+++ src/pubFiles_teste.php
@@ -0,0 +1,21 @@
+<?php
+
+$dir = dirname(__DIR__); //
+
+/* auxiliar para sh 
+	foreach (scandir("$dir/html") as $f) if (substr($f,-5,5)=='.html') {
+		$f2 = preg_replace('/^\d+-/','',$f);
+		$f2 = str_replace('.html','.htm',$f2);
+		echo "\ngit mv $f $f2";
+		//file_put_contents("html_clean/$f",$htm2);
+	}
+*/
+
+foreach (scandir("$dir/html") as $f) if (substr($f,-5,5)=='.htm') {
+	file_get_contents("html_clean/$f",$htm2);
+}
+
+
+?>
+
+
