249c2932df6bdeba7a90fe74bcbe723497dd9933
Peter Krauss <ppkrauss@gmail.com>
2016-10-14 07:40:37 -0300

    index para navegacao

--- src/pubFiles_teste.php
+++ src/pubFiles_teste.php
@@ -11,11 +11,8 @@ $dir = dirname(__DIR__); //
 	}
 */
 
-foreach (scandir("$dir/html") as $f) if (substr($f,-5,5)=='.htm') {
-	file_get_contents("html_clean/$f",$htm2);
-}
-
 
 ?>
+Resto no Javascript, via jQuery-load.
 
 
