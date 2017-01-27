cc96913aea5a24d4659e256e7ae368fbfb831eeb
Marco Zanchi <mzanchi@gmail.com>
2017-01-27 16:50:14 -0200

    adding extra information to tokens

--- src/collect_structures.py
+++ src/collect_structures.py
@@ -2,13 +2,13 @@ import getMateria
 import getHtm
 
 # Test script. This should get all docs from edition 1772.
-ediParam = 1772
+ediParam = 3335
 materias = getHtm.getedition(ediParam)
 for materia in materias:
     matParam = materia['matId']
-    tokens = getMateria.extract_tokens(materia['matEdi'], materia['matId'])
-    print('Outputting tokens to', '../tokens/' + str(ediParam) + '-' + str(matParam) + '.html')
-    f = open('../tokens/' + str(ediParam) + '-' + str(matParam) + '.txt', 'w')
+    tokens = getMateria.extract_tokens(materia)
+    print('Outputting tokens to ../tokens/' + str(ediParam) + '-' + str(matParam) + '.txt')
+    f = open('../tokens/' + str(ediParam) + '-' + str(matParam) + '.txt', 'wb')
     for item in tokens:
-        f.write(item.encode('utf-8') + '\n')
+        f.write(item.encode('utf-8') + '\r\n')
     f.close()
